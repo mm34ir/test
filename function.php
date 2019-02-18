a399fd6348cdd3751d9f083b2abfa5c81417b75a
mm4130 <mm41.ir@gmail.com>
2019-02-18 14:35:14 +0330

    Update function.php

--- function.php
+++ function.php
@@ -1,4 +1,24 @@
 <?php
+header('Content-Type: application/json');
+function fileGetContentsCurl($url){
+    $curl = curl_init();
+    $agent = 'Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2';
+    $header = array('Accept-Charset: UTF-8');
+    curl_setopt($curl, CURLOPT_FAILONERROR, TRUE);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($curl, CURLOPT_MAXREDIRS, 2);
+    curl_setopt($curl, CURLOPT_USERAGENT, $agent);
+    curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 3); 
+    curl_setopt($curl, CURLOPT_URL, $url);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
